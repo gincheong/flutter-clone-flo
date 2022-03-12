352d8264713e746c022e1e37a59ef803bb486934
gincheong2 <gincheong2@gmail.com>
2022-03-13 00:50:22 +0900

    feat(ProgressBar)
    - 음원 총 시간, 현재 시간을 표시하는 타임스탬프 표시
    - Slider의 thumb 비활성화

--- lib/pages/player/05.progress_bar/progress_bar.dart
+++ lib/pages/player/05.progress_bar/progress_bar.dart
@@ -1,4 +1,5 @@
 import 'package:clone_flo/getx/controller.dart';
+import 'package:clone_flo/pages/player/05.progress_bar/styles.dart';
 import 'package:clone_flo/styles/colors.dart';
 import 'package:clone_flo/styles/scaling.dart';
 import 'package:flutter/material.dart';
@@ -39,22 +40,47 @@ class _ProgressBarState extends State<ProgressBar> {
     c.audioPlayer.value.seek(Duration(milliseconds: value.toInt()));
   }
 
+  static makeTimestamp(int msec) {
+    String minString = (msec / 1000 ~/ 60).toString().padLeft(2, '0');
+    String secString = (msec ~/ 1000 % 60).toString().padLeft(2, '0');
+
+    return '$minString:$secString';
+  }
+
   @override
   Widget build(BuildContext context) {
+    String currentTimestamp = makeTimestamp(currentDurationMsec);
+    String totalTimestamp = makeTimestamp(totalDurationMsec);
+
     return Container(
+        width: scale(1050),
         margin: EdgeInsets.only(bottom: verticalScale(100)),
-        child: SliderTheme(
-            data: SliderTheme.of(context).copyWith(
-                trackHeight: verticalScale(10),
-                thumbColor: FloColors.transparent,
-                thumbShape:
-                    const RoundSliderThumbShape(enabledThumbRadius: 0.0)),
-            child: Slider(
-                activeColor: FloColors.accent,
-                inactiveColor: FloColors.disable,
-                thumbColor: FloColors.transparent,
-                value: currentDurationMsec.toDouble(),
-                max: totalDurationMsec.toDouble(),
-                onChanged: onChangedSlider)));
+        child: Column(children: [
+          SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                  trackHeight: verticalScale(10),
+                  overlayShape: SliderComponentShape.noThumb,
+                  thumbColor: FloColors.white,
+                  thumbShape: const RoundSliderThumbShape(
+                      elevation: 0,
+                      pressedElevation: 0,
+                      enabledThumbRadius: 0.0)),
+              child: SizedBox(
+                  height: verticalScale(70),
+                  child: Slider(
+                      activeColor: FloColors.accent,
+                      inactiveColor: FloColors.disable,
+                      thumbColor: FloColors.transparent,
+                      value: currentDurationMsec.toDouble(),
+                      max: totalDurationMsec.toDouble(),
+                      onChanged: onChangedSlider))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(currentTimestamp, style: Styles.currentTimestampText),
+              Text(totalTimestamp, style: Styles.totalTimestampText)
+            ],
+          )
+        ]));
   }
 }
---
+++ lib/pages/player/05.progress_bar/styles.dart
@@ -0,0 +1,8 @@
+import 'package:clone_flo/styles/colors.dart';
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const TextStyle currentTimestampText =
+      TextStyle(color: FloColors.accent);
+  static const TextStyle totalTimestampText = TextStyle(color: FloColors.gray);
+}
