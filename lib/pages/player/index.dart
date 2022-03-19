9400693523ec5248c2f1d5469e0e10f59b62d73d
gincheong2 <gincheong2@gmail.com>
2022-03-19 16:40:19 +0900

    fix: widget 공통 export 처리

---
+++ lib/pages/player/index.dart
@@ -0,0 +1,6 @@
+export 'package:clone_flo/pages/player/01.player_header/player_header.dart';
+export 'package:clone_flo/pages/player/02.song_info/song_info.dart';
+export 'package:clone_flo/pages/player/03.lyric_viewer/lyric_viewer.dart';
+export 'package:clone_flo/pages/player/04.like_button/like_button.dart';
+export 'package:clone_flo/pages/player/05.progress_bar/progress_bar.dart';
+export 'package:clone_flo/pages/player/06.player_controller/player_controller.dart';
--- lib/pages/player/player.dart
+++ lib/pages/player/player.dart
@@ -1,9 +1,4 @@
-import 'package:clone_flo/pages/player/01.player_header/player_header.dart';
-import 'package:clone_flo/pages/player/02.song_info/song_info.dart';
-import 'package:clone_flo/pages/player/03.lyric_viewer/lyric_viewer.dart';
-import 'package:clone_flo/pages/player/04.like_button/like_button.dart';
-import 'package:clone_flo/pages/player/05.progress_bar/progress_bar.dart';
-import 'package:clone_flo/pages/player/06.player_controller/player_controller.dart';
+import 'package:clone_flo/pages/player/index.dart';
 import 'package:clone_flo/styles/colors.dart';
 import 'package:flutter/material.dart';
 
